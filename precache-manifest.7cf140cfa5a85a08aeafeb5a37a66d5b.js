96bc88c78fc37da80e5264848469357abb9b907a
Tony Flores <florestony5454@gmail.com>
2020-04-23 02:15:51 -0500

    Updates

---
+++ precache-manifest.7cf140cfa5a85a08aeafeb5a37a66d5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa56aa7c0376a0b1af94959fa1f8059",
+    "url": "/budget-dashboard/index.html"
+  },
+  {
+    "revision": "819fbb66058d9d977e07",
+    "url": "/budget-dashboard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e5947cfbfd1f55b010db",
+    "url": "/budget-dashboard/static/css/main.c87490b8.chunk.css"
+  },
+  {
+    "revision": "819fbb66058d9d977e07",
+    "url": "/budget-dashboard/static/js/2.5568eec5.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/budget-dashboard/static/js/2.5568eec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5947cfbfd1f55b010db",
+    "url": "/budget-dashboard/static/js/main.4b1cd661.chunk.js"
+  },
+  {
+    "revision": "fad09946eecb985c6aad",
+    "url": "/budget-dashboard/static/js/runtime-main.840c874a.js"
+  }
+]);
